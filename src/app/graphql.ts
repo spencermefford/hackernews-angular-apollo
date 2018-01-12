fa0f0a18291667eeff8b4b6bc3fbda4f935ff700
Spencer Mefford <spencermefford@gmail.com>
2018-01-12 14:46:09 -0700

    First mutation

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -16,3 +16,22 @@ export interface AllLinkQueryResponse {
   allLinks: Link[];
   loading: boolean;
 }
+
+export const CREATE_LINK_MUTATION = gql`
+  mutation CreateLinkMutation($description: String!, $url: String!) {
+    createLink(
+      description: $description,
+      url: $url,
+    ) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+export interface CreateLinkMutationResponse {
+  createLink: Link;
+  loading: boolean;
+}
